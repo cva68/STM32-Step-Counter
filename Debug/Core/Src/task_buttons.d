eedebaf8c7302fd56312b2aaa172ef38564feb18
Connor Varney <cva68@uclive.ac.nz>
2025-04-19 15:58:48 +1200

    Removed unwanted files marked in .gitignore

--- Debug/Core/Src/task_buttons.d
+++
@@ -1,7 +0,0 @@
-Core/Src/task_buttons.o: ../Core/Src/task_buttons.c \
- ../Core/Inc/task_buttons.h ../Core/Inc/buttons.h ../Core/Inc/rgb.h \
- ../Core/Inc/buttons.h
-../Core/Inc/task_buttons.h:
-../Core/Inc/buttons.h:
-../Core/Inc/rgb.h:
-../Core/Inc/buttons.h:
